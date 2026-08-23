e850c69b2aa98d11c9dde0e2e58df0b5975f32c9
LuckyJadhav2808 <luckymanojjadhav@gmail.com>
2026-08-23 23:59:33 +0530

    feat(android): explicitly register native plugins in MainActivity.java

--- android/app/src/main/java/com/invictus/omnitracker/MainActivity.java
+++ android/app/src/main/java/com/invictus/omnitracker/MainActivity.java
@@ -1,5 +1,19 @@
 package com.invictus.omnitracker;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.capacitorjs.plugins.localnotifications.LocalNotificationsPlugin;
+import com.capacitorjs.plugins.haptics.HapticsPlugin;
+import com.capacitorjs.plugins.splashscreen.SplashScreenPlugin;
+import com.capacitorjs.plugins.statusbar.StatusBarPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(LocalNotificationsPlugin.class);
+        registerPlugin(HapticsPlugin.class);
+        registerPlugin(SplashScreenPlugin.class);
+        registerPlugin(StatusBarPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
