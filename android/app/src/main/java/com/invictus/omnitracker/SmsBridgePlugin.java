8a42a5b592b244de76e4964f76cafa1fa364b3a8
Lucky Manoj Jadhav <luckymanojjadhav@gmail.com>
2026-09-25 23:53:03 +0530

    fix(android): import Intent in SmsBridgePlugin for settings launch

--- android/app/src/main/java/com/invictus/omnitracker/SmsBridgePlugin.java
+++ android/app/src/main/java/com/invictus/omnitracker/SmsBridgePlugin.java
@@ -2,6 +2,7 @@ package com.invictus.omnitracker;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import androidx.core.content.ContextCompat;
